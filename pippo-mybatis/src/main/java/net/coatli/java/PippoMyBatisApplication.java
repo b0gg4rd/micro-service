bfd1dd69447b93ade8483a0916a908d1d357c53a
b0gg4rd <boggard@hotmail.com>
2017-03-09 00:09:37 -0600

    refactor(app): use route group

--- pippo-mybatis/src/main/java/net/coatli/java/PippoMyBatisApplication.java
+++ pippo-mybatis/src/main/java/net/coatli/java/PippoMyBatisApplication.java
@@ -1,13 +1,19 @@
 package net.coatli.java;
 
 import net.coatli.java.service.PersonMicroService;
+import ro.pippo.core.Application;
 import ro.pippo.core.Pippo;
 
-public class PippoMyBatisApplication {
+public class PippoMyBatisApplication extends Application {
 
   public static void main(final String[] args) {
 
-    new Pippo(new PersonMicroService()).start();
+    new Pippo(new Application() {
+      @Override
+      public void onInit() {
+        addRouteGroup(new PersonMicroService());
+      }
+    }).start();
 
   }
 }
--- pippo-mybatis/src/main/java/net/coatli/java/service/PersonMicroService.java
+++ pippo-mybatis/src/main/java/net/coatli/java/service/PersonMicroService.java
@@ -5,43 +5,31 @@ import static net.coatli.java.config.MyBatisConfig.sqlSessionFactory;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import net.coatli.java.domain.Person;
 import net.coatli.java.events.ResponseAllPersonsEvent;
 import net.coatli.java.events.ResponsePersonEvent;
 import net.coatli.java.persistence.PersonMapper;
-import ro.pippo.core.Application;
+import ro.pippo.core.route.RouteGroup;
 
-public class PersonMicroService extends Application {
+public class PersonMicroService extends RouteGroup {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(PersonMicroService.class);
+  public PersonMicroService() {
 
-  @Override
-  protected void onInit() {
+    super("/persons");
 
-    POST("/persons/", (routeContext) -> {
-
-      LOGGER.info("Creating person");
-
+    POST("/", (routeContext) -> {
       try (SqlSession sqlSession = sqlSessionFactory().openSession()) {
-
         sqlSession.getMapper(PersonMapper.class).create(routeContext.createEntityFromBody(Person.class));
         sqlSession.commit();
         routeContext.getResponse().created();
-
       }
-
     });
 
-    GET("/persons/{key}", (routeContext) -> {
-      LOGGER.info("Retrieving person");
-
+    GET("/{key}", (routeContext) -> {
       try (SqlSession sqlSession = sqlSessionFactory().openSession()) {
-
-        final Person person = sqlSession.getMapper(PersonMapper.class).retrieve(routeContext.getParameter("key").toLong());
-
+        final Person person = sqlSession.getMapper(PersonMapper.class).retrieve(
+            routeContext.getParameter("key").toLong());
         routeContext.json().send(
           new ResponsePersonEvent()
             .setDomainFound(person != null)
@@ -49,20 +37,14 @@ public class PersonMicroService extends Application {
       }
     });
 
-    GET("/persons/", (routeContext) -> {
-
-      LOGGER.info("Finding all persons.");
-
+    GET("/", (routeContext) -> {
       try (SqlSession sqlSession = sqlSessionFactory().openSession()) {
-
         final List<Person> persons = sqlSession.getMapper(PersonMapper.class).findAll();
-
         routeContext.json().send(
           new ResponseAllPersonsEvent()
             .setDomainFound(!persons.isEmpty())
             .setPersons(persons));
       }
-
     });
 
   }
