93f03e2605796804aac57e18c412b5fbfb50ebb6
b0gg4rd <boggard@hotmail.com>
2017-04-06 01:13:09 -0500

    reafactor(app): remove filters and add validations

--- pippo-mybatis/src/main/java/net/coatli/java/service/PersonMicroService.java
+++ pippo-mybatis/src/main/java/net/coatli/java/service/PersonMicroService.java
@@ -3,8 +3,11 @@ package net.coatli.java.service;
 import java.util.List;
 import java.util.UUID;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.coatli.java.config.MyBatisConfig;
 import net.coatli.java.domain.Person;
@@ -12,110 +15,169 @@ import net.coatli.java.event.RequestAllPersonsEvent;
 import net.coatli.java.mapper.PersonMapper;
 import ro.pippo.core.Application;
 import ro.pippo.core.HttpConstants;
+import ro.pippo.core.PippoRuntimeException;
 
 public class PersonMicroService extends Application {
 
-  private static final int NO_CONTENT = 204;
+  private static final Logger LOGGER = LoggerFactory.getLogger(PersonMicroService.class);
 
-  private static final String SQL_SESSION = "sqlSession";
+  private static final String CREATE_PATH   = "/persons/";
+  private static final String RETRIEVE_PATH = "/persons/{key}";
+  private static final String UPDATE_PATH   = "/persons/{key}";
+  private static final String DELETE_PATH   = "/persons/{key}";
+  private static final String FIND_ALL_PATH = "/persons/";
+  private static final String FIND_BY_PATH  = "/persons";
 
-  private static final String PERSON_MAPPER = "personMapper";
+  private static final int SUCCESS_CREATE = 1;
+  private static final int SUCCESS_UPDATE = 1;
+  private static final int SUCCESS_DELETE = 1;
 
   @Override
   protected void onInit() {
 
     final SqlSessionFactory sqlSessionFactory = new MyBatisConfig(getPippoSettings()).sqlSessionFactory();
 
-    // open sqlsession
-    ALL("/.*", (routeContext) -> {
-      final SqlSession sqlSession = sqlSessionFactory.openSession();
-
-      routeContext.setLocal(SQL_SESSION, sqlSession);
-      routeContext.setLocal(PERSON_MAPPER, sqlSession.getMapper(PersonMapper.class));
-
-      routeContext.next();
-    });
-
     // create
-    POST("/persons/", (routeContext) -> {
-      final SqlSession sqlSession = routeContext.getLocal(SQL_SESSION);
-      final PersonMapper personMapper = routeContext.getLocal(PERSON_MAPPER);
+    POST(CREATE_PATH, (routeContext) -> {
+      Person person = null;
 
-      final Person person = routeContext.createEntityFromBody(Person.class).setKey(UUID.randomUUID().toString());
+      try {
+        person = routeContext.createEntityFromBody(Person.class)
+                   .setKey(UUID.randomUUID().toString());
+      } catch (final PippoRuntimeException exc) {
+        LOGGER.error("Incorrect resource.", exc);
+        routeContext.status(HttpConstants.StatusCode.BAD_REQUEST);
+        return ;
+      }
 
-      personMapper.create(person);
-      sqlSession.commit();
+      final SqlSession sqlSession = sqlSessionFactory.openSession();
+      final PersonMapper personMapper = sqlSession.getMapper(PersonMapper.class);
 
-      routeContext.status(HttpConstants.StatusCode.CREATED).json().send(person);
+      if (SUCCESS_CREATE == personMapper.create(person)) {
+        sqlSession.commit();
+        routeContext.status(HttpConstants.StatusCode.CREATED).json().send(person);
+      } else {
+        sqlSession.rollback();
+        routeContext.status(HttpConstants.StatusCode.INTERNAL_ERROR);
+      }
     });
 
     // retrieve
-    GET("/persons/{key}", (routeContext) -> {
-      final PersonMapper personMapper = routeContext.getLocal(PERSON_MAPPER);
-      final Person person = personMapper.retrieve(routeContext.getParameter("key").toString());
+    GET(RETRIEVE_PATH, (routeContext) -> {
+
+      final String key = routeContext.getParameter("key").toString();
+
+      if (StringUtils.isBlank(key)) {
+        LOGGER.error("Incorrect key '{}'.", key);
+        routeContext.status(HttpConstants.StatusCode.BAD_REQUEST);
+        return ;
+      }
+
+      final SqlSession sqlSession = sqlSessionFactory.openSession();
+      final PersonMapper personMapper = sqlSession.getMapper(PersonMapper.class);
+
+      final Person person = personMapper.retrieve(key);
 
       if (person == null) {
-        routeContext.status(NO_CONTENT);
+        routeContext.status(HttpConstants.StatusCode.NO_RESPONSE);
       } else {
-        routeContext.json().send(person);
+        routeContext.status(HttpConstants.StatusCode.OK).json().send(person);
       }
     });
 
     // update
-    PUT("/persons/{key}", (routeContext) -> {
-      final SqlSession sqlSession = routeContext.getLocal(SQL_SESSION);
-      final PersonMapper personMapper = routeContext.getLocal(PERSON_MAPPER);
+    PUT(UPDATE_PATH, (routeContext) -> {
 
-      personMapper.update(
-          routeContext.createEntityFromBody(Person.class).setKey(routeContext.getParameter("key").toString()));
-      sqlSession.commit();
+      Person person = null;
 
-      routeContext.status(NO_CONTENT);
+      try {
+        person = routeContext
+            .createEntityFromBody(Person.class)
+            .setKey(routeContext.getParameter("key").toString());
+
+        if (StringUtils.isBlank(person.getKey())) {
+          LOGGER.error("Incorrect key '{}'.", person.getKey());
+          routeContext.status(HttpConstants.StatusCode.BAD_REQUEST);
+          return ;
+        }
+      } catch (final PippoRuntimeException exc) {
+        LOGGER.error("", exc);
+        routeContext.status(HttpConstants.StatusCode.BAD_REQUEST);
+        return ;
+      }
+
+      final SqlSession sqlSession = sqlSessionFactory.openSession();
+      final PersonMapper personMapper = sqlSession.getMapper(PersonMapper.class);
+
+      if (SUCCESS_UPDATE == personMapper.update(person)) {
+        sqlSession.commit();
+        routeContext.status(HttpConstants.StatusCode.NO_RESPONSE);
+      } else {
+        sqlSession.rollback();
+        routeContext.status(HttpConstants.StatusCode.INTERNAL_ERROR);
+      }
     });
 
     // delete
-    DELETE("/persons/{key}", (routeContext) -> {
-      final SqlSession sqlSession = routeContext.getLocal(SQL_SESSION);
-      final PersonMapper personMapper = routeContext.getLocal(PERSON_MAPPER);
+    DELETE(DELETE_PATH, (routeContext) -> {
+      final String key = routeContext.getParameter("key").toString();
 
-      personMapper.delete(routeContext.getParameter("key").toString());
-      sqlSession.commit();
+      if (StringUtils.isBlank(key)) {
+        LOGGER.error("Incorrect key '{}'.", key);
+        routeContext.status(HttpConstants.StatusCode.BAD_REQUEST);
+        return ;
+      }
 
-      routeContext.status(NO_CONTENT);
+      final SqlSession sqlSession = sqlSessionFactory.openSession();
+      final PersonMapper personMapper = sqlSession.getMapper(PersonMapper.class);
+
+      if (SUCCESS_DELETE == personMapper.delete(key)) {
+        sqlSession.commit();
+        routeContext.status(HttpConstants.StatusCode.NO_RESPONSE);
+      } else {
+        sqlSession.rollback();
+        routeContext.status(HttpConstants.StatusCode.INTERNAL_ERROR);
+      }
     });
 
     // findAll
-    GET("/persons/", (routeContext) -> {
-      final PersonMapper personMapper = routeContext.getLocal(PERSON_MAPPER);
+    GET(FIND_ALL_PATH, (routeContext) -> {
+
+      final SqlSession sqlSession = sqlSessionFactory.openSession();
+      final PersonMapper personMapper = sqlSession.getMapper(PersonMapper.class);
 
       final List<Person> persons = personMapper.findAll();
 
       if (persons.isEmpty()) {
-        routeContext.status(NO_CONTENT);
+        routeContext.status(HttpConstants.StatusCode.NO_RESPONSE);
       } else {
-        routeContext.json().send(persons);
+        routeContext.status(HttpConstants.StatusCode.OK).json().send(persons);
       }
     });
 
     // findBy
-    GET("/persons", (routeContext) -> {
-      final PersonMapper personMapper = routeContext.getLocal(PERSON_MAPPER);
+    GET(FIND_BY_PATH , (routeContext) -> {
+      RequestAllPersonsEvent filters = null;
 
-      final List<Person> persons = personMapper.findBy(
-          routeContext.createEntityFromParameters(RequestAllPersonsEvent.class));
+      try {
+        filters = routeContext.createEntityFromParameters(RequestAllPersonsEvent.class);
+      } catch(final PippoRuntimeException exc) {
+        LOGGER.error("Incorrect filters.", exc);
+        routeContext.status(HttpConstants.StatusCode.BAD_REQUEST);
+        return ;
+      }
+
+      final SqlSession sqlSession = sqlSessionFactory.openSession();
+      final PersonMapper personMapper = sqlSession.getMapper(PersonMapper.class);
+
+      final List<Person> persons = personMapper.findBy(filters);
 
       if (persons.isEmpty()) {
-        routeContext.status(NO_CONTENT);
+        routeContext.status(HttpConstants.StatusCode.NO_RESPONSE);
       } else {
-        routeContext.json().send(persons);
+        routeContext.status(HttpConstants.StatusCode.OK).json().send(persons);
       }
     });
-
-    // close sqlsession
-    ALL("/.*", (routeContext) -> {
-      final SqlSession sqlSession = routeContext.removeLocal(SQL_SESSION);
-      sqlSession.close();
-    }).runAsFinally();
 
   }
 
